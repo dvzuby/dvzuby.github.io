bfcbf02aaef0c271fbae3655dd3b3d02df62ef01
zev <dvzuby@aitrics.com>
2026-03-04 10:15:24 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	// 커스텀 도메인 사용 시: 'https://dvzuby.blog'
-	// 커스텀 도메인 없이 GitHub Pages 사용 시: 'https://dvzuby.github.io/dvzuby.blog'
-	site: 'https://dvzuby.github.io/dvzuby.blog',
-	// 커스텀 도메인 없이 GitHub Pages 사용 시 base 필요
-	// 커스텀 도메인 사용 시 아래 줄 삭제
-	base: '/dvzuby.blog',
+	site: 'https://dvzuby.github.io',
 	integrations: [mdx(), sitemap()],
 });
